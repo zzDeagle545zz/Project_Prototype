7fa2b1fdf4780bc89d53e181df3f58516235a9d9
Christopher Garcia <Deagles545@gmail.com>
2016-06-01 10:38:30 -0700

    Signed-off-by: Christopher Garcia <Deagles545@gmail.com>

---
+++ CreatureBattle/CreatureBattle/CreatureBattle/Element.h
@@ -0,0 +1,25 @@
+/*
+* File:   Ability.h
+* Author: zzDeagle545zz
+*
+* Created on April 29, 2016, 3:46 PM
+*/
+
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+//User Libraries
+
+////////////////////////////////////////////////////////////////////////////////
+class Element {
+private:
+//Damage Multipliers 
+	float eDmgx;		//Elemental Damage with same Element
+	float eResx;		//Elemental Resist against opposite Element
+	float etkdmgx;		//Elemental damage taken against strong Element
+public:
+
+
+
+};
+#endif	// ELEMENT_H
